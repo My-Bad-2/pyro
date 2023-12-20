4d631118e070bc78ff434ba220e0dcf0d65a27c7
My-Bad-2 <yashv7883@gmail.com>
2023-12-20 05:43:46 -0500

    Memory management functions

--- kernel/include/libc/stdlib.h
+++ kernel/include/libc/stdlib.h
@@ -129,6 +129,62 @@ unsigned long strtoul(const char* __restrict nptr, char** __restrict endptr,
 unsigned long long strtoull(const char* __restrict nptr,
                             char** __restrict endptr, int base);
 
+/// \brief Allocates a block of memory of the specified size.
+///
+/// This function allocates a block of memory of the specified size from the kernel memory pool.
+/// The allocated memory is not guaranteed to be zero-initialized.
+///
+/// \param size The size of the memory block to allocate.
+/// \return A pointer to the allocated memory block, or NULL if allocation fails.
+void* kmalloc(size_t size);
+
+/// \brief Changes the size of the memory block pointed to by ptr.
+///
+/// This function changes the size of the memory block pointed to by ptr to the specified size.
+/// The contents of the block are preserved up to the minimum of the old and new sizes.
+/// If the new size is larger, the additional memory is uninitialized.
+///
+/// \param ptr A pointer to the memory block to resize.
+/// \param size The new size for the memory block.
+/// \return A pointer to the reallocated memory block, or NULL if reallocation fails.
+void* krealloc(void* ptr, size_t size);
+
+/// \brief Allocates memory for an array of num elements of nmemb size each.
+///
+/// This function allocates memory for an array of num elements, each of size nmemb, from the
+/// kernel memory pool. The allocated memory is initialized to zero.
+///
+/// \param num The number of elements in the array.
+/// \param size The size of each element in the array.
+/// \return A pointer to the allocated and zero-initialized memory block, or NULL if allocation fails.
+void* kcalloc(size_t num, size_t size);
+
+/// \brief Frees the memory block pointed to by ptr.
+///
+/// This function frees the memory block pointed to by ptr, which must have been previously
+/// allocated using kmalloc, krealloc, or kcalloc.
+///
+/// \param ptr A pointer to the memory block to free. If ptr is NULL, no action is performed.
+void kfree(void* ptr);
+
+/// \brief Allocates a block of memory with specified size and alignment.
+///
+/// This function allocates a block of memory with the specified size and alignment from the
+/// kernel memory pool. The allocated memory is not guaranteed to be zero-initialized.
+///
+/// \param size The size of the memory block to allocate.
+/// \param alignment The alignment requirement for the memory block.
+/// \return A pointer to the allocated memory block, or NULL if allocation fails.
+void* kmemalign(size_t size, size_t alignment);
+
+/// \brief Frees the memory block pointed to by ptr, allocated with kmemalign.
+///
+/// This function frees the memory block pointed to by ptr, which must have been previously
+/// allocated using kmemalign.
+///
+/// \param ptr A pointer to the memory block to free. If ptr is NULL, no action is performed.
+void kmemalign_free(void* ptr);
+
 __END_CDECLS
 
 #endif  // KERNEL_INCLUDE_LIBC_STDLIB_H_
--- kernel/src/memory/heap.cpp
+++ kernel/src/memory/heap.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <system/log.h>
 
 #include <memory/heap.hpp>
@@ -27,3 +28,49 @@ void heap_initialize() {
     log_message(LOG_LEVEL_INFO, "Successfully Initialized Kernel Heap.");
 }
 }  // namespace memory
+
+/// \brief Allocates a block of memory of the specified size.
+///
+/// This function allocates a block of memory of the specified size from the kernel memory pool.
+/// The allocated memory is not guaranteed to be zero-initialized.
+///
+/// \param size The size of the memory block to allocate.
+/// \return A pointer to the allocated memory block, or NULL if allocation fails.
+void* kmalloc(size_t size) {
+    return memory::__heap_allocator->malloc(size);
+}
+
+/// \brief Changes the size of the memory block pointed to by ptr.
+///
+/// This function changes the size of the memory block pointed to by ptr to the specified size.
+/// The contents of the block are preserved up to the minimum of the old and new sizes.
+/// If the new size is larger, the additional memory is uninitialized.
+///
+/// \param ptr A pointer to the memory block to resize.
+/// \param size The new size for the memory block.
+/// \return A pointer to the reallocated memory block, or NULL if reallocation fails.
+void* krealloc(void* ptr, size_t size) {
+    return memory::__heap_allocator->realloc(ptr, size);
+}
+
+/// \brief Allocates memory for an array of num elements of nmemb size each.
+///
+/// This function allocates memory for an array of num elements, each of size nmemb, from the
+/// kernel memory pool. The allocated memory is initialized to zero.
+///
+/// \param num The number of elements in the array.
+/// \param size The size of each element in the array.
+/// \return A pointer to the allocated and zero-initialized memory block, or NULL if allocation fails.
+void* kcalloc(size_t num, size_t size) {
+    return memory::__heap_allocator->calloc(num, size);
+}
+
+/// \brief Frees the memory block pointed to by ptr.
+///
+/// This function frees the memory block pointed to by ptr, which must have been previously
+/// allocated using kmalloc, krealloc, or kcalloc.
+///
+/// \param ptr A pointer to the memory block to free. If ptr is NULL, no action is performed.
+void kfree(void* ptr) {
+    memory::__heap_allocator->free(ptr);
+}
